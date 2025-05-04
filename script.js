175d3225982649baaef0b759784b3241d85c0543
Asumba-lab <steveasumba@gmail.com>
2025-05-04 18:30:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("colorBtn").addEventListener("click", () => {
   document.body.style.backgroundColor = randomColor;
   
   // Add a fun message
-  const messages = ['🌈 Wow!', '🎨 Beautiful!', '✨ Amazing!', '🌟 Stunning!'];
+  const messages = [' Wow!', ' Beautiful!', '✨ Amazing!', ' Stunning!'];
   const randomMessage = messages[Math.floor(Math.random() * messages.length)];
   alert(randomMessage);
 });
